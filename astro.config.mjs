6a6211cffca0d3accc1405a3953bb75c30517c15
orbulant <vydranix@gmail.com>
2024-11-02 18:48:24 +0800

    Overhauled Everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.tankashing.com',
+  image: {
+    // Example: Allow remote image optimization from a single domain
+    domains: ['http://github-profile-summary-cards.vercel.app'],
+  },
+});
